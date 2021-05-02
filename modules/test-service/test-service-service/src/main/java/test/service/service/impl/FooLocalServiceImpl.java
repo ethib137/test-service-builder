3dd10399add70183f0ab7ff8e6b6bb12005c444f
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-05-02 10:04:16 +0400

    test

---
+++ modules/test-service/test-service-service/src/main/java/test/service/service/impl/FooLocalServiceImpl.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package test.service.service.impl;
+
+import com.liferay.portal.aop.AopService;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import org.osgi.service.component.annotations.Component;
+
+import test.service.exception.NoSuchFooException;
+import test.service.model.Foo;
+import test.service.service.base.FooLocalServiceBaseImpl;
+import test.service.service.persistence.FooPersistence;
+
+/**
+ * The implementation of the foo local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>test.service.service.FooLocalService</code> interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
+ * </p>
+ *
+ * @author Brian Wing Shun Chan
+ * @see FooLocalServiceBaseImpl
+ */
+@Component(
+	property = "model.class.name=test.service.model.Foo",
+	service = AopService.class
+)
+public class FooLocalServiceImpl extends FooLocalServiceBaseImpl {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Use <code>test.service.service.FooLocalService</code> via injection or a <code>org.osgi.util.tracker.ServiceTracker</code> or use <code>test.service.service.FooLocalServiceUtil</code>.
+	 */
+
+	public Foo getFoo(long fooId) throws PortalException {
+		return fooPersistence.findByPrimaryKey(fooId);
+	}
+
+	public Foo deleteFoo(long fooId) throws NoSuchFooException {
+		return fooPersistence.remove(fooId);
+	}
+}
